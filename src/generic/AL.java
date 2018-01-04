498819585acb8e09c999bc7ded21183e78d4faec
Clever <Clever@github.com>
2018-01-05 00:36:12 +0530

    Initial commit

---
+++ src/generic/AL.java
@@ -0,0 +1,51 @@
+package generic;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.util.Date;
+import java.util.Properties;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+
+public class AL {
+	public static String getProperty(String path,String Key)
+	{
+		String v="";
+		try {
+			Properties p=new Properties();
+			p.load(new FileInputStream(path));
+			v=p.getProperty(Key);
+		}
+		catch(Exception e)
+		{
+			
+		}
+		
+		
+		return v;
+		
+	}
+	
+	public static String getPhoto(WebDriver driver,String folder,String name)
+	{
+		TakesScreenshot t=(TakesScreenshot)driver;
+		File srcFile=t.getScreenshotAs(OutputType.FILE);
+		String dt=new Date().toString().replaceAll(":","_");
+		String path=folder+name+dt+".png";
+		File destFile=new File(path);
+		try {
+			FileUtils.copyFile(srcFile, destFile);
+		}
+		catch(Exception e)
+		{
+			
+		}
+		
+		return path;
+		
+	}
+	
+}
---
+++ src/generic/IAutoConst.java
@@ -0,0 +1,13 @@
+package generic;
+
+public interface IAutoConst {
+	
+	String CHROME_KEY="webdriver.chrome.driver";
+	String CHROME_VALUE="./Driver/chromedriver.exe";
+	String GECKO_KEY="webdriver.gecko.driver";
+	String GECKO_VALUE="./Driver/geckodriver.exe";
+	String XL_PATH="./Data/input.xlsx";
+	String SETTINGS_PATH="./settings.properties";
+	String PHOTO_PATH="./Photo/";
+
+}
